234d9e150197afa52c4f797010b1f8638818209c
TemExile <hxwengl@163.com>
2019-05-10 14:06:35 +0800

    finishing the exercise on matrix and correct some function done by 5/9

--- week-01/day-5/matrix.py
+++ week-01/day-5/matrix.py
@@ -1,18 +1,15 @@
-m1 = [[1,2,3],
-      [4,5,6],
-      [7,8,9]]
-m2 = [[4,3,2],
-      [2,2,9],
-      [2,5,7]]
-
-m3 = [[1,3],[3,2]]
-m4 = [[3,4],[1,1]]
+m1 = [[1,2,3], [4,5,6], [7,8,9]]
+m2 = [[4,3,2], [2,2,9], [2,5,7]]
+m3 = [[1,3], [3,2]]
+m4 = [[3,4], [1,1]]
+m5 = m1*3
+m6 = m3*4
 
 # matrix addition
 def matrix_add(a,b):
     d = []
     if len(a) != len(b) | len(a[0]) != len(b[0]):
-        return print("They have different demension")
+        return "They have different demension"
     for i in range(len(a)):
         c = [x + y for x, y in zip(a[i], b[i])]
         d.append(c)
@@ -24,7 +21,7 @@ print(matrix_add(m3,m4))
 def matrix_sub(a,b):
     d = []
     if len(a) != len(b) | len(a[0]) != len(b[0]):
-        return print("They have different demension")
+        return "They have different dimensions"
     for i in range(len(a)):
         c = [x - y for x, y in zip(a[i], b[i])]
         d.append(c)
@@ -45,9 +42,92 @@ print(scale_mul(m1, 2))
 print(scale_mul(m2, 3))
 
 # Transposition
+def tran(m):
+    a = []
+    for i in range(len(m)):
+        b = []
+        for j in range(len(m)):
+            b.append(m[j][i])
+        a.append(b)
+    return a
+print(tran(m1))
+
+# Transposition_faster
 def transp(m):
     return list(map(list, zip(*m)))
 print(transp(m1))
+
+# multiplication
+def mul(m,n):
+    if len(m[0]) != len(n):
+        return "You cannot do multiplication for this two matrices"
+    else:
+        a = []
+        for i in range(len(m)):
+            b = []
+            for j in range(len(n[0])):
+                c = 0
+                for k in range(len(m[0])):
+                    c = c + m[i][k] * n[k][j]
+                b.append(c)
+            a.append(b)
+        return a
+print(mul(m1, m2))
+print(mul([[1,2,3],[3,2,1]],[[1,2],[3,4],[2,1]]))
+
+# vertical flipping
+def v_flip(m):
+    if len(m)%2 == 0:
+        a = len(m) // 2
+        b = m[:a]
+        c = m[a:]
+        return c+b
+    elif len(m)%2 != 0:
+        a = len(m) // 2
+        b = m[:a]
+        c = m[a+1:]
+        return c+[m[a]]+b
+print(v_flip(m5))
+print(v_flip(m6))
+
+# horizontal flipping
+def h_flip(m):
+    a = []
+    for i in range(len(m)):
+        b = m[i]
+        c = b[::-1]
+        a.append(c)
+    return a
+print(h_flip(m5))
+
+# main anti-diagonal mirroring
+def anti_dia(m):
+    if len(m) != len(m[0]):
+        return "This mattrix is not a square matrix"
+    else:
+        a = transp(m)
+        b = v_flip(a)
+        c = h_flip(b)
+        return c
+print(anti_dia(m1))
+
+# matrix rotation
+def rotate(m, cl = 'clockwise'):
+    if cl == 'clockwise':
+        a = transp(m)
+        b = v_flip(a)
+        return b
+    elif cl == 'anti':
+        a = transp(m)
+        b = h_flip(a)
+        return b
+print(rotate(m1, 'anti'))
+print(rotate(m1, 'clockwise'))
+
+
+
+
+
 
 
 
