88cc5c60322e694c3d480b6ee0f431391f829388
TemExile <hxwengl@163.com>
2019-05-13 15:45:23 +0800

    correct the code in sharpieset

--- week-02/day-1/sharpie.py
+++ week-02/day-1/sharpie.py
@@ -11,18 +11,29 @@ exp1.use(31.3)
 print(exp1.ink_amount)
 
 class sharpieset(object):
-    def __init__(self, list):
-        self.list = list
+    def __init__(self):
+        self.list = []
+    def adding(self):
+        self.list.append(sharpie('white',1.2,23))
+        self.list.append(sharpie('black',5.0,12))
+        self.list.append(sharpie('yellow',12.0,0))
+        self.list.append(sharpie('black',2.0,40))
+        self.list.append(sharpie('blue',1.0,0))
     def count_usable(self):
         a = 0
         for i in range(len(self.list)):
-            if sharpie.ink_amount(self.list[i]) > 0:
+            if self.list[i].ink_amount > 0:
                 a += 1
         return a
     def remove_trash(self):
         b = []
         for i in range(len(self.list)):
-            if sharpie.ink_amount(self.list[i]) > 0
+            if self.list[i].ink_amount > 0:
                 b.append(self.list[i])
-        return b
+            return b
+
+aaa = sharpieset()
+aaa.adding()
+print(aaa.count_usable())
+
 
