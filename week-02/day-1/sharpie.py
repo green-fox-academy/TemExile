f26666dfcdbb4299dddfcc7a10b1922516022fec
TemExile <hxwengl@163.com>
2019-05-13 14:48:43 +0800

    add sharpieset class, no test yet

--- week-02/day-1/sharpie.py
+++ week-02/day-1/sharpie.py
@@ -9,3 +9,20 @@ class sharpie(object):
 exp1 = sharpie(color = 'white', width = 2.3)
 exp1.use(31.3)
 print(exp1.ink_amount)
+
+class sharpieset(object):
+    def __init__(self, list):
+        self.list = list
+    def count_usable(self):
+        a = 0
+        for i in range(len(self.list)):
+            if sharpie.ink_amount(self.list[i]) > 0:
+                a += 1
+        return a
+    def remove_trash(self):
+        b = []
+        for i in range(len(self.list)):
+            if sharpie.ink_amount(self.list[i]) > 0
+                b.append(self.list[i])
+        return b
+
