78fface45621140cac1e2d1204ab8c73941f8a63
TemExile <hxwengl@163.com>
2019-05-13 15:07:06 +0800

    added some example to test the code

--- week-02/day-1/thing.py
+++ week-02/day-1/thing.py
@@ -6,3 +6,11 @@ class Thing:
         self.completed = True
     def __str__(self):
         return ("[x] " if self.completed else "[ ] ") + self.name
+
+thing1 = Thing('Get milk')
+thing2 = Thing('Remove the obstacles')
+thing3 = Thing('Stand up')
+thing4 = Thing('Eat lunch')
+thing3.complete()
+thing4.complete()
+print(thing1,thing2, thing3, thing4)
