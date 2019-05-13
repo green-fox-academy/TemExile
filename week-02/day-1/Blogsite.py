fa266d80319d482461b2aed6266c5f65f29c3a7d
TemExile <hxwengl@163.com>
2019-05-13 16:48:18 +0800

    added Blog class to BlogPost exercise

--- week-02/day-1/Blogsite.py
+++ week-02/day-1/Blogsite.py
@@ -10,3 +10,22 @@ exp1 = BlogPost('John Doe', 'Lorem Ipsum', 'Lorem iposum dolor sit amet.',
 exp2 = BlogPost('Tim Urban', 'Wait but why',
                 'A popular long-form, stick-figure-illustrated blog about almost everything.',
                 '2010.10.10')
+
+class Blog(object):
+    def __init__(self, list):
+        self.list = list
+    def adding(self, post):
+        self.list.append(post)
+    def deleted(self, index):
+        self.list.pop(index)
+    def updated(self, index, newpost):
+        self.list.insert(index, newpost)
+
+b = [exp1, exp2]
+c = Blog(b)
+c.adding(BlogPost('asdf','1111','444','1223'))
+print(c.list)
+c.deleted(2)
+print(c.list)
+c.updated(2,BlogPost('abcc','cdwe','444','1235'))
+print(c.list)
