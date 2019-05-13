73546f3bb15bd4e55e23c537dc95810d7c000324
TemExile <hxwengl@163.com>
2019-05-13 15:08:00 +0800

    added the code to get the same output

--- week-02/day-1/fleet_of_things.py
+++ week-02/day-1/fleet_of_things.py
@@ -2,3 +2,14 @@ from fleet import Fleet
 from thing import Thing
 
 fleet = Fleet()
+thing1 = Thing('Get milk')
+thing2 = Thing('Remove the obstacles')
+thing3 = Thing('Stand up')
+thing4 = Thing('Eat lunch')
+thing3.complete()
+thing4.complete()
+fleet.add(thing1)
+fleet.add(thing2)
+fleet.add(thing3)
+fleet.add(thing4)
+print(fleet)
