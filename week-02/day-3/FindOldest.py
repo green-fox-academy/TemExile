273c3d7fc2985de4ae76dbdc79ee3cef806b15c5
TemExile <hxwengl@163.com>
2019-05-15 10:13:12 +0800

    built the function to find the oldest movie

---
+++ week-02/day-3/FindOldest.py
@@ -0,0 +1,16 @@
+# oldest movie
+def findoldest(file):
+    a = open(file, 'r')
+    b = a.readlines()
+    c = []
+    for line in b:
+        d = line.split(';')
+        c.append(d)
+    e = []
+    for i in range(len(c)):
+        e.append(int(c[i][1]))
+        f = e.index(min(e))
+    return c[f][0]
+
+
+print(findoldest('movies.txt'))
