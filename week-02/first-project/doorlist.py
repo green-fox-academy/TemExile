aea0bf8a66e76ae62057f815d50d3ccd1f6ec387
TemExile <hxwengl@163.com>
2019-05-19 11:58:28 +0800

    the dictionary for main door, second floor and third floor's door

---
+++ week-02/first-project/doorlist.py
@@ -0,0 +1,5 @@
+def maindoor():
+    doorlist = {'A66 - 04 F??BEJ??RAT (F-1) Door #1':'main',
+    'A66 - 12 Recepci?? (2-1) Door #1': 'second', 
+    'A66 - 18 El??t??r 316 (3-2) Door #1': 'third'}
+    return doorlist
