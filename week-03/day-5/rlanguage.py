3faa3c9147236856d1a18f66eaf75247e47511a5
TemExile <hxwengl@163.com>
2019-05-24 10:00:53 +0800

    the exercise for showing different language as the page loaded

---
+++ week-03/day-5/rlanguage.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def load_language():
+    with open('language.txt', encoding = 'UTF-8', mode = 'r') as infile:
+        greet = infile.readlines()
+    x = random.randint(0,len(greet)-1)
+    code = render_template('language.html', greeints = greet[x])
+    return code
+
+if __name__ == '__main__':
+    app.run(debug=True)
