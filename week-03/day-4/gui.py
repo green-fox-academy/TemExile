f63947bb1ad805609aa73600ac457d8a9efc3ff8
TemExile <hxwengl@163.com>
2019-05-24 14:09:28 +0800

    finished the gui exercises, added the modify/edit page

--- week-03/day-4/gui.py
+++ week-03/day-4/gui.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/movie')
 def first_page():
-    return render_template('first_page.html')
+    with open('gui.json', 'r') as infile:
+        a = json.load(infile)
+    return render_template('first_page.html', movielist = a)
 
 @app.route('/')
 def redi():
@@ -14,6 +16,10 @@ def redi():
 @app.route('/add-movie')
 def add_movie():
     return render_template('add.html')
+
+@app.route('/edit/<movie_id>')
+def edit(movie_id):
+    return render_template('edit.html', movie_id = movie_id)
 
 @app.route('/api/movies', methods = ['POST'])
 def get_data():
@@ -40,7 +46,39 @@ def get_data():
         resp = make_response(response, 403)
         return resp
 
-# @app.route('/api/movies/<movie_id>')
+@app.route('/api/movies/<movie_id>', methods = ['POST'])
+def change(movie_id):
+    ids = movie_id
+    title = request.form['title']
+    year = request.form['year']
+    genre = request.form['genre']
+    des = request.form['description']
+    code = request.form['code']
+    newmovie = {'id':ids, 'title':title, 'year':year, 'genre':genre, 'description':des}
+    enterCode = '123'
+    if code == enterCode:
+        with open('gui.json', 'r') as infile:
+            a = json.load(infile)
+        response = jsonify(error = f'No movie found wiht {movie_id} ID')
+        resp = make_response(response, 404)
+        for l in a:
+            if newmovie['id'] == l["id"]:
+                l["title"] = newmovie["title"]
+                l["year"] = newmovie["year"]
+                l["genre"] = newmovie["genre"]
+                l["description"] = newmovie["description"]
+                with open('movie.json', 'w') as infile:
+                    json.dumps(a, infile)
+                response = jsonify(Result = "Request completed")
+                resp = make_response(response, 200)
+            else:
+                pass
+        return resp
+    else:
+        response = jsonify(error = 'Invalid Access Code')
+        resp = make_response(response, 403)
+        return resp
+
 
 
 if __name__ == '__main__':
