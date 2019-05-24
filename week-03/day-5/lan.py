f9ec4ca43e678b5427147b91bde5b883596eb02a
TemExile <hxwengl@163.com>
2019-05-24 10:15:51 +0800

    exercise on random language with random name add, along with files.

---
+++ week-03/day-5/lan.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def load_language():
+    with open('lan.txt', encoding = 'UTF-8', mode = 'r') as infile:
+        greet = infile.readlines()
+    x = random.randint(0,len(greet)-1)
+    with open('name.txt', encoding = 'UTF-8', mode = 'r') as infile:
+        name = infile.readlines()
+    y = random.randint(0, len(name)-1)
+    code = render_template('lang.html', greeting = greet[x], name = name[y])
+    return code
+
+if __name__ == '__main__':
+    app.run(debug=True)
