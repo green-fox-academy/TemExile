ee13bf4f78a50adfcd3c70a45a04da6713a2662f
TemExile <hxwengl@163.com>
2019-05-28 15:31:31 +0800

    finished all the question on the section

--- week-04/day-2/movie-rating-extra.sql
+++ week-04/day-2/movie-rating-extra.sql
@@ -25,6 +25,22 @@ WHERE title not in (SELECT a.title
                       join Reviewer b on b.rID = c.rID
               WHERE b.name = 'Chris Jackson');
 
+-- Q5
+SELECT distinct a.name, b.name
+FROM Reviewer a, Reviewer b, Movie, Rating c, Rating d
+WHERE c.mID = d.mID and 
+    c.rID = a.rID and 
+    d.rID = b.rID and
+    c.rID != d.rID and
+    a.name < b.name
+ ORDER BY a.name, b.name;
+
+-- Q6
+SELECT Reviewer.name, Movie.title, Rating.stars
+FROM Reviewer join Rating on Reviewer.rID = Rating.rID
+          join Movie on Movie.mID = Rating.mID
+WHERE Rating.stars = (SELECT min(Rating.stars) FROM Rating);
+
 -- Q7
 SELECT a.title, avg(b.stars) as rating
 FROM Movie a join Rating b on a.mID = b.mID
@@ -36,3 +52,37 @@ SELECT a.name
 FROM Reviewer a join Rating b on a.rID = b.rID
 GROUP BY b.rID
 HAVING count(b.rID) >= 3;
+
+-- Q9
+SELECT a.title, a.director
+FROM Movie a, Movie b
+WHERE a.title != b.title and 
+    a.director = b.director
+ORDER BY a.director, a.title;
+
+-- Q10
+SELECT Movie.title, avg(Rating.stars) as rate
+FROM Movie join Rating on Movie.mID = Rating.mID
+GROUP BY Movie.title
+Having rate >= (SELECT max(rate) FROM(
+                          SELECT avg(stars) as rate
+                          FROM Rating
+                          GROUP BY mID));
+
+-- Q11
+SELECT Movie.title, avg(Rating.stars) as rate
+FROM Movie join Rating on Movie.mID = Rating.mID
+GROUP BY Movie.title
+Having rate <= (SELECT min(rate) FROM(
+                          SELECT avg(stars) as rate
+                          FROM Rating
+                          GROUP BY mID));
+
+-- Q12
+SELECT a.director, Movie.title, a.rate
+FROM (SELECT Movie.director, max(Rating.stars) as rate, Movie.mID
+    FROM Movie join Rating on Movie.mID = Rating.mID
+    WHERE Movie.director is not NULL
+    GROUP BY Movie.director
+    ) as a, Movie
+WHERE a.mID = Movie.mID;
