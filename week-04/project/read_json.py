2a0d3f3bf765fe284a2520f000973d8ddac2d12a
TemExile <hxwengl@163.com>
2019-05-30 15:13:13 +0800

    execute the function to insert the data into database

---
+++ week-04/project/read_json.py
@@ -0,0 +1,17 @@
+import json
+import psycopg2
+import insertmessage
+import insertmentions
+import insertuser
+import insertreaction
+
+with open('gfa-team-thanks.json', 'r') as jinfile:
+    raw_data = json.load(jinfile)
+
+# remove the record that is a file_comment
+raw_data.pop(599)
+
+insertmessage.add_to_message(raw_data)
+insertuser.add_to_users(raw_data)
+insertreaction.add_to_reactions(raw_data)
+insertmentions.add_to_mention(raw_data)
