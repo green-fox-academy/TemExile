de23aed28ffeab986f2b75b88e8c992806c63b2c
TemExile <hxwengl@163.com>
2019-05-31 13:36:51 +0800

    added some views

--- week-04/project/supportsql.sql
+++ week-04/project/supportsql.sql
@@ -27,3 +27,32 @@ SELECT a.id as Message_ID, count(b.id) as reacctions
 FROM messages a left join reactions b on a.id = b.message_id
 GROUP BY a.id, b.message_id
 ORDER BY count(b.id) DESC;
+
+-- Who sent the most message
+SELECT user_id, count(id) as messages
+FROM messages
+GROUP BY user_id
+ORDER BY count(id) DESC;
+
+-- Which emoji is the most common as reaction
+SELECT reaction, count(id) as used
+FROM reactions
+GROUP BY reaction
+ORDER BY used DESC limit 10;
+
+-- Who reacted to the most messages
+SELECT user_id, count(id) as reactions
+FROM reactions
+GROUP BY user_id
+ORDER BY reactions DESC limit 10;
+
+
+SELECT avg(messages)
+FROM (SELECT user_id, count(id) as messages
+        FROM messages
+        GROUP BY user_id) as table1;
+
+SELECT avg(reactions)
+FROM (SELECT user_id, count(id) as reactions
+        FROM reactions
+        GROUP BY user_id) as table1;
