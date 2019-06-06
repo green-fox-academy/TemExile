e8a320bc26fe1948444c1d7f3aa8e67c7fe4a2a7
TemExile <hxwengl@163.com>
2019-06-06 15:13:51 +0800

    change some parameters

--- week-05/day-4/airline.py
+++ week-05/day-4/airline.py
@@ -1,16 +1,22 @@
 def pricing_function(days_left, tickets_left, demand_level):
-    if demand_level < 150:
-        if days_left > 3:
+    if demand_level < 165 and days_left <= 2:
+        if tickets_left > 20:
+            price = demand_level - int(tickets_left * 0.4)
+        else:
+            price = demand_level - tickets_left
+    elif demand_level < 165 and days_left > 2:
+        if tickets_left > 20:
             price = demand_level + 1
         else:
-            price = demand_level - 5
-    elif demand_level in range(150, 165):
-        if days_left > 3:
-            price = demand_level - 5
+            price = demand_level + 1
+    elif demand_level > 165 and days_left <= 2:
+        if tickets_left > 20:
+            price = demand_level - int(tickets_left * 0.7)
         else:
-            price = demand_level - 10
-    elif demand_level in range(165, 180):
-        price = demand_leve - 20
-    else:
-        price = demand_level - 50
+            price = demand_level - int(tickets_left * 0.5)
+    elif demand_level > 165 and days_left > 2:
+        if tickets_left > 20:
+            price = demand_level - int(tickets_left * 0.5)
+        else:
+            price = demand_level - tickets_left
     return price
