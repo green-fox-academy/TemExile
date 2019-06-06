5f6b766882b3e0e32e0939131aac1f474cc21390
TemExile <hxwengl@163.com>
2019-06-06 15:01:08 +0800

    added some question

--- week-05/day-2/question.sql
+++ week-05/day-2/question.sql
@@ -15,3 +15,10 @@ SELECT temperature FROM raw_weather_data
             day = 1 and 
             year = 2008 and 
             hour >= 9 and hour <=1
+
+-- question 4
+
+-- question 5
+SELECT id, name, max(lat), state_code, country_code 
+FROM weather_station WHERE state_code = 'TX' ALLOW FILTERING
+
