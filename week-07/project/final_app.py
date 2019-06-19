3c5eb9a6bfb66c4d738e0de029875352cbcfb1e9
TemExile <hxwengl@163.com>
2019-06-19 17:25:55 +0800

    flask app file

---
+++ week-07/project/final_app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+from function import get_bed, get_year, get_loc, fit_model, new_function, get_hold, get_home
+import pandas as pd 
+import numpy as np 
+from sklearn.linear_model import LinearRegression
+import math
+
+app = Flask(__name__)
+@app.route('/')
+def first_page():
+    return render_template('first_page.html')
+
+@app.route('/quote', methods = ['POST'])
+def quote_page():
+    Bedroom = request.form['Bedroom']
+    HoldType = request.form['HoldType']
+    HomeType = request.form['HomeType']
+    NewBuilding = request.form['NewBuilding']
+    postcode = request.form['postcode']
+    lat, long = get_loc(postcode)
+    Hold = get_hold(HoldType)
+    Home = get_home(HomeType)
+    New = new_function(NewBuilding)
+    input_x = [[int(Bedroom), Hold, Home, New, lat, long]]
+    model = fit_model()
+    y_pred = model.predict(input_x)
+    price = round(math.exp(y_pred),2)
+    return render_template('quote.html', price = price)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
